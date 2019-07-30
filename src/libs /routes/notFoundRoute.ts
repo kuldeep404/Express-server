c22081b850c5a4629d7137d4dd4f9faabc4fa42a
Kuldeep Kumar <kuldeep.kumar@lapr117.successivetech.com>
2019-07-30 13:19:25 +0530

    modified files

--- src/libs /routes/notFoundRoute.ts
+++
@@ -1,4 +0,0 @@
-export const middleWare1 = ( req, res, next) => {
-    console.log('inside middleware 1');
-    next('Not Found');
-};
--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,20 @@
 import * as bodyParser from 'body-parser';
 import * as  express from 'express';
-import { middeleWare2 } from './libs /routes/errorHandler';
-import { middleWare1 } from './libs /routes/notFoundRoute';
+import { errorHandlerMiddleware } from './libs/routes/errorHandler';
+import { notFoundRouteMiddleware } from './libs/routes/notFoundRoute';
 
 const app = express();
 
 export default class Server {
     constructor(private config) {
-        this.setupRoutes();
         this.run();
-        this.bootstrap();
-        this.initBodyParser();
-
     }
     public setupRoutes() {
         app.use('/health-check' , ( req, res ) => {
-        res.send('I am ok');
+            res.send('I am ok');
         });
-        app.use(middleWare1);
-        app.use('/api', middeleWare2);
+        app.use(notFoundRouteMiddleware);
+        app.use(errorHandlerMiddleware);
     }
     public bootstrap() {
         this.setupRoutes();
