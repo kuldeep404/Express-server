d82f0e87dfd8de94fac73da1a5eab8371b262856
Kuldeep Kumar <kuldeep.kumar@lapr117.successivetech.com>
2019-08-05 16:43:07 +0530

    features/40009 files modified

--- src/libs/routes/authmiddleware.ts
+++ src/libs/routes/authmiddleware.ts
@@ -3,16 +3,26 @@ import {hasPermission} from '../../../extraTs/utils';
 import * as jwt from 'jsonwebtoken';
 export default (moduleName,permissionType) => (req,res,next) => {
     const token = req.headers['authorization'];
-    const user = jwt.verify(token ,config.secretKey);
-    const role=user.role;
+
+    jwt.verify(token ,config.secretKey,(error,result)=>{
+            if (error){
+                next('Unauthrised Access');
+            }
+            else if (hasPermission(moduleName,result.role,permissionType)){
+                next()
+            }
+            else{
+                next("Unauthrised Access");
+
+            }
+            
+
+    });
+    //const role=user.role;
     // console.log("token is",token)
     // console.log("user info",user)
 
-    if(hasPermission(moduleName,role,permissionType)){
-        next();
-    }
-    else {
-        // console.log(" not found")
-        next("Unauthrised Access");
-    }
+    
+
+    
 }
