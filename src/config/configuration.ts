fabd52c2a77687954f74c8344e954c807d0dde8d
Kuldeep Kumar <52992496+kuldeep404@users.noreply.github.com>
2019-09-02 12:13:48 +0530

    Changes in indentation.

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -3,9 +3,9 @@ import { IConfig } from './IConfig';
 config();
 const envVars = process.env;
 const configuration: IConfig = Object.freeze({
-mongoUri: envVars.MONGO_URL,
-password: envVars.PASSWORD,
-port: envVars.PORT,
-secretKey: envVars.SECRET_KEY,
+  mongoUri: envVars.MONGO_URL,
+  password: envVars.PASSWORD,
+  port: envVars.PORT,
+  secretKey: envVars.SECRET_KEY,
 });
 export default configuration;
